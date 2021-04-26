1f66b64ae4bb33898c3e3c12874008e5bceed577
eabasquliyev <elgun.abasquliyev2@gmail.com>
2021-04-26 23:47:31 +0400

    Add custom controls

---
+++ PubApp/Views/CustomControls/BoolVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PubApp.Views.CustomControls
+{
+    public class BoolVisibilityConverter:IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            string hasText = (string) values[0];
+            bool hasFocus = (bool) values[1];
+
+            if (!String.IsNullOrWhiteSpace(hasText) || hasFocus)
+                return Visibility.Collapsed;
+
+            return Visibility.Visible;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PubApp/Views/CustomControls/SearchBox.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PubApp.Views.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for SearchBox.xaml
+    /// </summary>
+    public partial class SearchBox : UserControl
+    {
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(SearchBox));
+
+
+
+
+        public Brush Background
+        {
+            get { return (Brush)GetValue(BackgroundProperty); }
+            set { SetValue(BackgroundProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Background.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BackgroundProperty =
+            DependencyProperty.Register("Background", typeof(Brush), typeof(SearchBox));
+
+
+
+        public SearchBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
